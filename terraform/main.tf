9ae5d9cb7d740eb815a82985a78104ac4aa82c95
TimokhinR <144680525+TimokhinR@users.noreply.github.com>
2025-01-16 19:52:38 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,14 @@
-terraform {
-  required_version = ">=0.13.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+# Генерація випадкового ID для забезпечення унікальності імені групи безпеки
+resource "random_id" "suffix" {
+  byte_length = 8
 }
 
-# Configure the AWS provider
-provider "aws" {
-  region     = "eu-central-1"
-}
-
+# Створення безпекової групи з унікальним ім'ям
 resource "aws_security_group" "web_app" {
-  name        = "web_app"
-  description = "security group"
+  name        = "web_app_${random_id.suffix.hex}"
+  description = "Security group for web app"
+  vpc_id      = "vpc-083d3020ef74c0d98" 
+
   ingress {
     from_port   = 80
     to_port     = 80
@@ -23,12 +16,13 @@ resource "aws_security_group" "web_app" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- ingress {
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 65535
@@ -36,21 +30,7 @@ resource "aws_security_group" "web_app" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags= {
+  tags = {
     Name = "web_app"
   }
-}
-
-resource "aws_instance" "webapp_instance" {
-  ami           = "ami-0669b163befffbdfc"
-  instance_type = "t2.micro"
-  security_groups= ["web_app"]
-  tags = {
-    Name = "webapp_instance"
-  }
-}
-
-output "instance_public_ip" {
-  value     = aws_instance.webapp_instance.public_ip
-  sensitive = true
 }
